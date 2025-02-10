090678c06e066b0a7f1df41fcb70a23b21501bd9
Aabishkar Shrestha <Aabishkar@adsaro.com>
2025-02-10 12:59:37 +0545

    updated search footer

--- resources/views/front/common/footer.blade.php
+++ resources/views/front/common/footer.blade.php
@@ -116,7 +116,7 @@
                 <div class="row">
                     <div class="col-lg-6 col-12">
                         <div class="left">
-                            <p>Copyright © {{ date('Y') }} <a href="" target="_blank">Suvlav</a> - All
+                            <p class="text-black">Copyright © {{ date('Y') }} <span class="text-black">Suvlav </span> - All
                                 Rights Reserved.</p>
                         </div>
                     </div>
