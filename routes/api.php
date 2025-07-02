9dab52ebc17cff27255da25f7ec0db80f63314b7
Aabishkar Shrestha <Aabishkar@adsaro.com>
2025-07-02 13:00:40 +0545

    Updated Api for auth section

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,6 @@ Route::get('/user', function (Request $request) {
 Route::post('/customer/signup', [UserAuthController::class, 'register']);
 Route::post('/customer/login', [UserAuthController::class, 'login']);
 Route::post('/customer/checkotp/{member}', [UserAuthController::class, 'checkotp']);
+Route::post('/forgotpasswords', [UserAuthController::class, 'forgotpasswords'])->name('forgotpasswords');
+Route::post('/changepassword', [UserAuthController::class, 'changepassword'])->name('changepassword');
+Route::post('/resendotp', [UserAuthController::class, 'resendOtp'])->name('resendotp');
