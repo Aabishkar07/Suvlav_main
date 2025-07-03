40025dcfaaf382eef1bbce0308b032692606aade
anupkasula3 <anupkasula354@gmail.com>
2025-07-03 11:09:55 +0545

    fix bug

--- routes/api.php
+++ routes/api.php
@@ -13,15 +13,12 @@ Route::get('/user', function (Request $request) {
 Route::post('/customer/signup', [UserAuthController::class, 'register']);
 Route::post('/customer/login', [UserAuthController::class, 'login']);
 Route::post('/customer/checkotp/{member}', [UserAuthController::class, 'checkotp']);
-<<<<<<< HEAD
 Route::post('/forgotpasswords', [UserAuthController::class, 'forgotpasswords'])->name('forgotpasswords');
 Route::post('/changepassword', [UserAuthController::class, 'changepassword'])->name('changepassword');
 Route::post('/resendotp', [UserAuthController::class, 'resendOtp'])->name('resendotp');
-=======
 
 Route::get('/allproduct', [ApiProductController::class, 'allproduct']);
 Route::get('/product/{product}', [ApiProductController::class, 'singlepage']);
 Route::get('/allcategory', [ApiCategoryController::class, 'allcategory']);
 Route::get('/category/{category}', [ApiCategoryController::class, 'singlecategory']);
 Route::get('/categorywiseproduct/{category}', [ApiCategoryController::class, 'categorywiseproduct']);
->>>>>>> ea71a7019e9985d9b12439397bcc2481bfaafb48
