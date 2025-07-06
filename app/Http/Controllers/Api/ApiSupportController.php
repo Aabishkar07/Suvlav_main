597e221e1e8c0a62d657569b8640c0e866981636
Aabishkar Shrestha <Aabishkar@adsaro.com>
2025-07-06 17:39:58 +0545

    Updated api

--- app/Http/Controllers/Api/ApiSupportController.php
+++ app/Http/Controllers/Api/ApiSupportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Email;
 use App\Models\Faq;
+use App\Models\Page;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -22,26 +23,47 @@ class ApiSupportController extends Controller
         ], 200);
     }
 
-public function getemail(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-        'email' => 'required|email|unique:emails,email',
-    ]);
+    public function getemail(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:emails,email',
+        ]);
 
-    if ($validator->fails()) {
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()->first(),
+            ], 400);
+        }
+        $email = Email::create([
+            'email' => $request->email,
+        ]);
+
         return response()->json([
-            'status' => 'error',
-            'message' => $validator->errors()->first(),
-        ], 400);
+            'status' => '200',
+            'message' => 'Email saved successfully!',
+            'data' => $email,
+        ], 200);
     }
-    $email = Email::create([
-        'email' => $request->email,
-    ]);
+
+
+
+public function getprivacypolicy()
+{
+    $privacypolicy = Page::where('id', 4)->first();
+
+    if (!$privacypolicy) {
+        return response()->json([
+            'status' => 404,
+            'message' => 'Privacy policy not found'
+        ], 404);
+    }
 
     return response()->json([
-        'status' => '200',
-        'message' => 'Email saved successfully!',
-        'data' => $email,
+        'status' => 200,
+        'data' => $privacypolicy
     ], 200);
 }
+
+
 }
